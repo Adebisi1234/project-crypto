a6db5395a0bef706283564f211f82b9a8a1b49cb
Tobiloba Adebisi <oluwatobilobaadebisi6@gmail.com>
2022-09-14 16:38:28 +0000

    Finish desktop layout adding features later

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
-<script>
-  AOS.init();
-</script>
+// let violet = document.querySelectorAll(".crypto")
+
+// for (let i = 0; i < violet.length; i++) {
+//   violet[i].addEventListener('click', toggle)
+//   function toggle() {
+//     for(let j = 0; j < violet.length; j++){
+//       if(j != i){
+//         violet[j].classList.add('bg-white')
+//       }else {
+//         violet[i].classList.toggle('bg-violet')
+//       }
+  
+      
+//     }
+    
+//   }
+// }
+
